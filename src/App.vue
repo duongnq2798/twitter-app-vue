fd6cf73a5cb4ec6e493bec90b7ca74d782591726
duongnq2798 <duongnq27.98@gmail.com>
2020-07-29 23:05:48 +0700

    Twitter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex container h-screen w-full">
+    <!-- Side Nav -->
+    <div class="w-1/5 border-r border-lighter px-8 py-2">
+      <button class="h-12 w-12 hover:lighblue text-3xl rounded-full text-blue">
+        <i class="fab fa-twitter"></i>
+      </button>
+      <div >
+        <button v-for="(tab,index) in tabs" v-bind:key="index" @click="id = tab.id" :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${ id === tab.id ? 'text-blue' : ''}`">
+          <i :class="`${ tab.icon} text-2xl mr-4 text-left`"></i>
+          <p class="text-lg font-semibold text-left"> {{ tab.title }} </p>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      tabs: [
+        {icon: 'fas fa-home', title: 'Home', id: 'home'},
+        {icon: 'fas fa-hashtag', title: 'Explore', id: 'explore'},
+        {icon: 'far fa-bell', title: 'Notifications', id: 'notifications'},
+        {icon: 'far fa-envelope', title: 'Messages', id :'messages'},
+        {icon: 'far fa-bookmark', title: 'Bookmark', id: 'bookmarks'},
+        {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
+        {icon: 'far fa-user', title: 'Profile', id: 'profile'},
+        {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
+      ],
+      id: 'home'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
